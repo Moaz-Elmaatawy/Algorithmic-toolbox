97e62a0e1922395026af129f48adc2fb5ecacc92
moaz886 <56744153+moaz886@users.noreply.github.com>
2019-11-12 23:54:59 +0200

    Add files via upload

---
+++ week4_divide_and_conquer/1_binary_search/binary_search.cpp
@@ -0,0 +1,136 @@
+#include <iostream>
+#include <cassert>
+#include <vector>
+#include <algorithm>
+using namespace std ;
+
+int binary2(vector<int> v,int x){
+    if(binary_search(v.begin(),v.end(),x)){
+        int z=lower_bound(v.begin(),v.end(),x) - v.begin();
+        return z;
+    }
+    else{
+        return -1;
+    }
+}
+// 1 2 3 4 5 --
+// 0 1 2 3 4 5
+
+int binary(const vector<int> a, int x) {
+  int left = 0, right = (int)a.size()-1;
+  int mid=(right+left)/2;
+  int index=0;
+  while(true){
+    if(a[mid]>x){
+        right=mid-1;
+    }
+    else if(a[mid]<x){
+        left=mid+1;
+    }
+    else{break;}
+
+  if(mid==right||mid==left){
+    mid = -1;
+    break;
+  }
+  mid=(right+left)/2;
+  }
+  return mid;
+}
+
+int recursive_bs(vector<int> v,int left ,int right ,int x){
+    int mid =(left+right)/2;
+    if(x==v[mid]){
+        return mid;
+    }
+    if(right<=left){
+        return -1;
+    }
+    if (x > v[mid]){
+        left =mid+1;
+        return recursive_bs(v,left,right,x);
+    }
+   if (x<v[mid]){
+        right =mid-1;
+        return recursive_bs(v,left,right,x);
+    }
+}
+
+
+int linear_search(const vector<int> &a, int x) {
+  for (size_t i = 0; i < a.size(); ++i) {
+    if (a[i] == x) return i;
+  }
+  return -1;
+}
+/*
+//////////////////
+int bi(vector<int>arr, int l, int r, int x)
+{
+    if (r >= l) {
+        int mid = l + (r - l) / 2;
+
+        // If the element is present at the middle
+        // itself
+        if (arr[mid] == x)
+            return mid;
+
+        // If element is smaller than mid, then
+        // it can only be present in left subarray
+        if (arr[mid] > x)
+            return bi(arr, l, mid - 1, x);
+
+        // Else the element can only be present
+        // in right subarray
+        return bi(arr, mid + 1, r, x);
+    }
+
+    // We reach here when element is not
+    // present in array
+    return -1;
+}
+*/
+/////////////////////
+
+int divideandconquar(vector<int>v,int left ,int right ,int x){
+    if (left>right){
+        return -1;
+    }
+    int mid =(left +right)/2;
+    if (v[mid]==x){
+        return mid;
+    }
+    else if(x>v[mid]){
+        return divideandconquar(v,mid+1,right,x);
+    }
+    else{
+        return divideandconquar(v,left,mid-1,x);
+    }
+}
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for (size_t i = 0; i < a.size(); i++) {
+    cin >> a[i];
+  }
+  int m;
+  cin >> m;
+  vector<int> b(m);
+  for (int i = 0; i < m; ++i) {
+    cin >> b[i];
+  }
+    int left=0 ,right=b.size();
+  for (int i = 0; i < m; ++i) {
+    cout << divideandconquar(a,0,n-1,b[i]) << " ";
+    //cout << recursive_bs(a,0,n-1,b[i]) << " ";
+    //left =0;right=b.size();
+  }/*
+cout<<endl;
+    for (int i = 0; i < m; ++i) {
+    cout << binary2(a, b[i]) << " ";
+    //cout << recursive_bs(a,0,n,b[i]) << " ";
+    //left =0;right=b.size();
+  }*/
+}
---
+++ week4_divide_and_conquer/2_majority_element/majority_element.cpp
@@ -0,0 +1,42 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int get_majority_element(vector<int> &a, int left, int right) {
+  if (left == right) return -1;
+  if (left + 1 == right) return a[left];
+  //write your code here
+  return -1;
+}
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for (size_t i = 0; i < a.size(); ++i) {
+    cin >> a[i];
+  }
+  int max0=0 ,count0 =1 , max1=0 ,count1=0;
+  sort(a.begin(),a.end());
+  for (int i =0 ;i<a.size();++i){
+    if(a[i]==a[i+1]){
+        ++ count0 ;
+    }
+    else{
+        if(count0>count1){
+            max0=a[i];
+            count1=count0;
+        }
+        count0=1;
+    }
+  }
+  if(count1>n/2){
+    cout<<"1";
+  }
+  else{
+    cout<<"0";
+  }
+ // cout<<"max="<<max0<<","<<count1;
+    //cout << (get_majority_element(a, 0, a.size()) != -1) << '\n';
+}
